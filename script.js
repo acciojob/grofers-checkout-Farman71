b56cb8795c5dc2cb24233085d1a199e4c1f019a7
Farman71 <farmanhussain71@gmail.com>
2025-01-29 17:06:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let prices=document.querySelectorAll(".price");
+	let total=0;
+	prices.forEach(priceele=>{
+		total=total+parseFloat(priceele.textContent);
+	});
+	let row=document.createElement("tr");
+	let totCell=document.createElement("td")
+	totCell.colSpan=2;
+	totCell.textContent=`Total is : ${total}`;
+	  row.appendChild(totCell);
+	 const table = document.querySelector("table");
+  table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
